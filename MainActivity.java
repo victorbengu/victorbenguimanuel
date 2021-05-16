21ac3abcf3727fa7c5344cc4536fdca4d4aa19eb
victorbengu <83929429+victorbengu@users.noreply.github.com>
2021-05-16 14:03:13 +0100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -7,33 +7,29 @@ import android.widget.EditText;
 import android.widget.Button;
 import android.widget.TextView;
 
-    public class MainActivity extends AppCompatActivity {
-        Button bt1,bt2;
+public class MainActivity extends AppCompatActivity {
+    Button bt1,bt2;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            bt1 = findViewById(R.Id.bt_um);
-            bt2 = findViewById(R.Id.bt_dois);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt1 = findViewById(R.Id.bt_um);
+        bt2 = findViewById(R.Id.bt_dois);
 
-            bt1.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View view){
-                    Toast.makeText(context:MainActivity.this,  text:"botao clicado",Toast.LENGTH_SHORT).show();
-                }
+        bt1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Toast.makeText(context:MainActivity.this,  text:"botao clicado",Toast.LENGTH_SHORT).show();
+            }
 
-            });
-            bt2.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View view){
-                    Intent i = new Intent (packageContext:MainActivity.this,MainActivity2.class) ;
-                    startActivity(i);
-                }
+        });
+        bt2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i = new Intent (packageContext:MainActivity.this,MainActivity2.class) ;
+                startActivity(i);
+            }
 
-            });
-
-
-
-
-
+        });
+    }
